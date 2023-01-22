9fb9845cff6ef3db25c928d2e250a43c5c167ea2
KeshalHetti <kaneeshakeshal@gmail.com>
2023-01-22 09:33:47 +0530

    removed unwanted pages

--- src/router.jsx
+++ src/router.jsx
@@ -11,8 +11,6 @@ import Support from './pages/Support'
 import OverView from './pages/Overview'
 import Guides from './pages/Guides'
 import Faqs from './pages/Faqs'
-import BlogDetail from './pages/BlogDetail'
-import Blog from './pages/Blog'
 import Contact from './pages/Contact'
 import UploadWork from './pages/UploadWork'
 import Collections from './pages/Collections'
@@ -26,7 +24,6 @@ import ItemDetailOne from './pages/ItemDetailOne'
 import ItemDetailTwo from './pages/ItemDetailTwo'
 import Auction from './pages/Auction'
 import DarkVersionOne from './pages/DarkVersionOne'
-import BlogSidebar from './pages/BlogSidebar'
 import ExploreOne from './pages/ExploreOne'
 import CreatorProfileEdit from './pages/CreatorProfileEdit'
 import Terms from './pages/Terms'
@@ -55,21 +52,14 @@ export default function Router() {
         <Route exact path="/helpcenter-guides" element={<Guides />} />
         <Route exact path="/helpcenter-faqs" element={<Faqs />} />
 
-        {/* template page routes  */}
-        <Route exact path="/blog-detail" element={<BlogDetail />} />
+        {/*page routes  */}
         <Route exact path="/aboutus" element={<AboutUs />} />
-        <Route exact path="/blogs" element={<Blog />} />
-        <Route exact path="/blog-sidebar" element={<BlogSidebar />} />
         <Route exact path="/contact" element={<Contact />} />
         <Route exact path="/upload-work" element={<UploadWork />} />
         <Route exact path="/collections" element={<Collections />} />
         <Route exact path="/become-creator" element={<BecomeCreator />} />
         <Route exact path="/creator-profile" element={<CreateProfile />} />
-        <Route
-          exact
-          path="/creator-profile-edit"
-          element={<CreatorProfileEdit />}
-        />
+        <Route exact path="/creator-profile-edit" element={<CreatorProfileEdit />} />
         <Route exact path="/creators" element={<Creator />} />
         <Route exact path="/wallet" element={<Wallet />} />
         <Route exact path="/activity" element={<Activity />} />
@@ -77,45 +67,11 @@ export default function Router() {
         <Route exact path="/item-detail-two" element={<ItemDetailTwo />} />
         <Route exact path="/auction" element={<Auction />} />
         <Route exact path="/explore-one" element={<ExploreOne />} />
-
         <Route exact path="/index-dark" element={<DarkVersionOne />} />
         <Route exact path="/index-dark-rtl" element={<DarkVersionOne />} />
         <Route exact path="/index" element={<DarkVersionOne />} />
         <Route exact path="/" element={<DarkVersionOne />} />
         <Route exact path="/index-rtl" element={<DarkVersionOne />} />
-
-      {/*   <Route exact path="/index-two-dark" element={<DarkVersionTwo />} />
-        <Route exact path="/index-two-dark-rtl" element={<DarkVersionTwo />} />
-        <Route exact path="/index-two" element={<DarkVersionTwo />} />
-        <Route exact path="/index-two-rtl" element={<DarkVersionTwo />} />
-
-        <Route exact path="/index-three-dark" element={<DarkVersionThree />} />
-        <Route
-          exact
-          path="/index-three-dark-rtl"
-          element={<DarkVersionThree />}
-        />
-        <Route exact path="/index-three" element={<DarkVersionThree />} />
-        <Route exact path="/index-three-rtl" element={<DarkVersionThree />} />
-
-        <Route exact path="/index-four-dark" element={<DarkVersionFour />} />
-        <Route
-          exact
-          path="/index-four-dark-rtl"
-          element={<DarkVersionFour />}
-        />
-        <Route exact path="/index-four" element={<DarkVersionFour />} />
-        <Route exact path="/index-four-rtl" element={<DarkVersionFour />} />
-
-        <Route exact path="/index-five-dark" element={<DarkVersionFive />} />
-        <Route
-          exact
-          path="/index-five-dark-rtl"
-          element={<DarkVersionFive />}
-        />
-        <Route exact path="/index-five" element={<DarkVersionFive />} />
-        <Route exact path="/index-five-rtl" element={<DarkVersionFive />} /> */}
-
         <Route exact path="/terms" element={<Terms />} />
         <Route exact path="/privacy" element={<Privacy />} />
         <Route exact path="/changelog" element={<ChangeLog />} />
