85b72eb1a8a5aed592c00307d26b0218f0152164
KeshalHetti <kaneeshakeshal@gmail.com>
2023-01-26 08:20:57 +0530

    Added Fuzzy Search

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,39 +1,55 @@
-import React, { useState } from 'react';
-import { TextField, IconButton, InputAdornment } from '@mui/material';
-import { Search as SearchIcon } from '@mui/icons-material';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Fuse from 'fuse.js';
+import {db} from '../../firebase-config'
+import { collection, getDocs } from "firebase/firestore"; 
 
 const SearchBar = () => {
-  const [searchText, setSearchText] = useState('');
+    const [search, setSearch] = useState('');
+    const [items, setItems] = useState([]);
+    const itemsCollectionRef = collection(db, 'items');
+    useEffect(() => {
+        const getItems = async () => {
+            const data = await getDocs(itemsCollectionRef)
+            setItems(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+        }
+        getItems();
+    }, [])
+        
+        const fuse = new Fuse(items, {
+            keys: ['name']
+            })
 
-  const handleSearch = (event) => {
-    event.preventDefault();
-    console.log(`Searching for ${searchText}`);
-  };
+        const results = fuse.search(search)
+        console.log(results)
 
-  return (
-    <form onSubmit={handleSearch}>
-      <TextField
-        label="Search"
-        variant="outlined"
-        value={searchText}
-        onChange={(event) => setSearchText(event.target.value)}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <SearchIcon />
-            </InputAdornment>
-          ),
-          endAdornment: (
-            <InputAdornment position="end">
-              <IconButton type="submit" aria-label="search">
-                <SearchIcon />
-              </IconButton>
-            </InputAdornment>
-          ),
-        }}
-      />
-    </form>
-  );
-};
+        const mapResults = results.map((results) => {
+            return (
+                <div className="card nft-items nft-primary rounded-md shadow overflow-hidden mb-1 p-3 card-body content position-relative p-0 mt-3">
+                    <br/>
+                    <h5>Item Name: {results.item.name}</h5>
+                    <h5>Item Price: {results.item.price}</h5>
+                    <h5>Item Quantity: {results.item.quantity}</h5>
+                </div>
+            )
+        });
 
-export default SearchBar;
+    const handleChange = (event) => {
+        setSearch(event.target.value);
+      };
+
+    return (       
+    <div>
+        <TextField
+                id="outlined-name"
+                label="Search an item"
+                value={search}
+                onChange={handleChange}
+            />
+            <h4>{search.length > 3 && mapResults}</h4>
+    </div>
+  )
+}
+
+export default SearchBar
